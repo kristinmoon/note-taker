af95f576011b2ae8f59877420911b0b51f397949
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-12-14 21:53:50 -0700

    add middleware

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const path = require('path');
 
 const { notes } = require('./Develop/db/db.json');
 
+// MIDDLEWARE
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// define paths to corresponding front-end files in the public folder like css and images
+app.use(express.static(path.join(__dirname, './Develop/public')));
+
 function createNewNote(body, notesArray) {
   const note = body;
   notesArray.push(note);
