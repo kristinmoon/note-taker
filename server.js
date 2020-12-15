c6cb00cb4c59afa33b92bfb4c12bc3bce9ab63ec
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-12-14 20:52:18 -0700

    connect server with html

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+const PORT = process.env.PORT || 3001;
+
 const express = require('express');
 const app = express();
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+const fs = require('fs');
+const path = require('path');
+
+const { notes } = require('./Develop/db/db.json');
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './Develop/db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
+}
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
